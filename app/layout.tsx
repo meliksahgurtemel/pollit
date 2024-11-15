e1c9abc530c8db4583227b24ea212b71a611c42d
Meliksah Gurtemel <meliksahgurtemel01@gmail.com>
2024-11-16 03:01:21 +0700

    added eruda

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ErudaProvider } from '@/components/Eruda'
 import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,12 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black text-white min-h-screen pb-16`}>
-        <main className="max-w-md mx-auto px-4">
-          {children}
-        </main>
-        <Navbar />
-      </body>
+      <ErudaProvider>
+        <body className={`${inter.className} bg-black text-white min-h-screen pb-16`}>
+          <main className="max-w-md mx-auto px-4">
+            {children}
+          </main>
+          <Navbar />
+        </body>
+      </ErudaProvider>
     </html>
   )
 }
