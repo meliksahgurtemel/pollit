b55602d578996ce3757d368879a1d0b347ed0293
Meliksah Gurtemel <meliksahgurtemel01@gmail.com>
2024-11-16 03:09:23 +0700

    added minikit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ErudaProvider } from '@/components/Eruda'
+import MiniKitProvider from '@/components/MinikitProvider'
 import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'PollIt',
+  title: 'Poll It',
   description: 'Participate polls and earn tokens',
 }
 
@@ -19,12 +20,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ErudaProvider>
-        <body className={`${inter.className} bg-black text-white min-h-screen pb-16`}>
-          <main className="max-w-md mx-auto px-4">
-            {children}
-          </main>
-          <Navbar />
-        </body>
+        <MiniKitProvider>
+          <body className={`${inter.className} bg-black text-white min-h-screen pb-16`}>
+            <main className="max-w-md mx-auto px-4">
+              {children}
+            </main>
+            <Navbar />
+          </body>
+        </MiniKitProvider>
       </ErudaProvider>
     </html>
   )
---
+++ components/MinikitProvider.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { MiniKit } from "@worldcoin/minikit-js"
+import { ReactNode, useEffect } from "react"
+
+export default function MiniKitProvider({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    MiniKit.install()
+    console.log(MiniKit.isInstalled())
+  }, []);
+
+  return <>{children}</>
+}
