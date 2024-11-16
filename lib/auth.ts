bf4a10a0ac3c2d89dac666df7561a3b38325d304
Meliksah Gurtemel <meliksahgurtemel01@gmail.com>
2024-11-16 13:32:42 +0700

    removed supabase

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,32 +1,36 @@
 import { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
-    providers: [
-      {
-        id: "worldcoin",
-        name: "Worldcoin",
-        type: "oauth",
-        wellKnown: "https://id.worldcoin.org/.well-known/openid-configuration",
-        authorization: { params: { scope: "openid" } },
-        clientId: process.env.WLD_CLIENT_ID,
-        clientSecret: process.env.WLD_CLIENT_SECRET,
-        idToken: true,
-        checks: ["state", "nonce", "pkce"],
-        profile(profile) {
-          return {
-            id: profile.sub,
-            name: profile.sub,
-            verificationLevel:
-              profile["https://id.worldcoin.org/v1"].verification_level,
-          };
-        },
-      },
-    ],
-    callbacks: {
-      async jwt({ token, user }) {
-        token.userRole = 'admin'
-        return token
+  providers: [
+    {
+      id: "worldcoin",
+      name: "Worldcoin",
+      type: "oauth",
+      wellKnown: "https://id.worldcoin.org/.well-known/openid-configuration",
+      authorization: { params: { scope: "openid" } },
+      clientId: process.env.WLD_CLIENT_ID,
+      clientSecret: process.env.WLD_CLIENT_SECRET,
+      idToken: true,
+      checks: ["state", "nonce", "pkce"],
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.sub,
+          verificationLevel:
+            profile["https://id.worldcoin.org/v1"].verification_level,
+        };
       },
     },
-    debug: true,
-  };
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      token.userRole = 'user'
+      return token
+    },
+  },
+  debug: true,
+};
