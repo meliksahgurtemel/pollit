a3186be6eb1d2814d807a6a12bcf7d0c7e0e05dc
Meliksah Gurtemel <meliksahgurtemel01@gmail.com>
2024-11-17 02:48:44 +0700

    disable cache

--- app/api/leaderboard/route.ts
+++ app/api/leaderboard/route.ts
@@ -1,6 +1,9 @@
 import { adminDb } from "@/app/api/lib/firebase-admin";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export async function GET() {
   try {
     const usersRef = adminDb.collection('users');
